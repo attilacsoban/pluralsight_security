02e890f4386c2519f43af881cc0cf6929de9896e
Csobban <csoban.attila@openminds.hu>
2015-06-11 11:24:27 +0200

    password storage completed

---
+++ src/main/java/test/PasswordHash.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import junit.framework.TestCase;
+
+public class PasswordHash extends TestCase {
+
+	public void testMD5Hash(){
+		String password = "attila";
+		Md5PasswordEncoder passEncoder = new Md5PasswordEncoder();
+		String hashedPassword = passEncoder.encodePassword(password, null);
+		System.out.println(hashedPassword);
+		
+	}
+	
+	public void testBCryptHash(){
+		String password = "attila";
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String hashedPassword = encoder.encode(password);
+		System.out.println(hashedPassword);
+	}
+	
+}
